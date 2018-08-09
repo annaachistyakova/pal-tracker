98cec9e4ce2d1002ca4fe670ba0fb17844ad5879
AnnaChistyakova <anna.chistyakova@perficient.com>
2018-08-09 16:51:32 -0400

    uncommented class

--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -1,4 +1,4 @@
-/*
+
 package test.pivotal.pal.trackerapi;
 
 import io.pivotal.pal.tracker.PalTrackerApplication;
@@ -25,4 +25,3 @@ public class WelcomeApiTest {
         assertThat(body).isEqualTo("Hello from test");
     }
 }
-*/
